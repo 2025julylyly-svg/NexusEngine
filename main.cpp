2238d52a9cc01dc14f9c101a3c69eab0cb8defa4
Develepment <2025julylyly@gmail.com>
2026-08-15 01:01:12 +0330

    refactor(ReadInput): update key state checking methods

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ int main() {
             break;
         }
         input.Reset();
+        std::cout << "---------------------------------------" << std::endl;
         std::this_thread::sleep_for( std::chrono::milliseconds(100) );
     }
     return 0;
