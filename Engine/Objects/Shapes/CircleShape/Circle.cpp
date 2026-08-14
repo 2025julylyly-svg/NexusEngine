087f6676e36cf0443885a0bad677ca9b46a439ec
Develepment <2025julylyly@gmail.com>
2026-08-14 23:43:06 +0330

    feat(Engine/Objects/Shapes/CircleShape): implement Circle class with movement and styling functionalities

---
+++ Engine/Objects/Shapes/CircleShape/Circle.cpp
@@ -0,0 +1,39 @@
+#include "Circle.h"
+
+Circle::Circle(float x, float y) : Speed( 1.f ) {
+    circle.setPosition( { x, y } );
+}
+
+void Circle::SetSpeed(float speed) {
+    Speed = speed;
+}
+
+void Circle::SetColor(uint8_t r, uint8_t g, uint8_t b, float state) {
+    if (state == 0) {
+        circle.setFillColor( { r, g, b } );
+    } else if (state == 1) {
+        circle.setFillColor( sf::Color::Transparent );
+        circle.setOutlineThickness( state );
+        circle.setOutlineColor( { r, g, b } );
+    }
+}
+
+void Circle::SetRadius(float Radius) {
+    circle.setRadius( Radius );
+}
+
+void Circle::MoveUp() {
+    this->circle.setPosition( { this->circle.getPosition().x, this->circle.getPosition().y - this->Speed } );
+}
+
+void Circle::MoveDown() {
+    this->circle.setPosition( { this->circle.getPosition().x, this->circle.getPosition().y + this->Speed } );
+}
+
+void Circle::MoveLeft() {
+    this->circle.setPosition( { this->circle.getPosition().x - this->Speed, this->circle.getPosition().y } );
+}
+
+void Circle::MoveRight() {
+    this->circle.setPosition( { this->circle.getPosition().x + this->Speed, this->circle.getPosition().y } );
+}
---
+++ Engine/Objects/Shapes/CircleShape/Circle.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Circle final
+{
+private:
+    sf::CircleShape circle;;
+    float Speed;
+public:
+    explicit Circle(float /* X : position */, float /* Y : position */);
+    void SetSpeed(float );
+    void SetColor(uint8_t, uint8_t, uint8_t, float /* state : without outliner if state == 0 */);
+    void SetRadius(float);
+    void MoveUp();
+    void MoveDown();
+    void MoveLeft();
+    void MoveRight();
+};
