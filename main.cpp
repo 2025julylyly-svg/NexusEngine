9c24d8ca3bbc15f80d1002f57061aec0187bfe4b
Develepment <2025julylyly@gmail.com>
2026-08-15 00:44:47 +0330

    feat(main.cpp): add key release and exit functionality

--- main.cpp
+++ main.cpp
@@ -17,6 +17,12 @@ int main() {
         else if (input.IsKeyHeld( 30 )) {
             std::cout << "A : Key Held" << std::endl;
         }
+        else if (input.IsKeyReleased( 30 )) {
+            std::cout << "A : Key Released" << std::endl;
+        }
+        else if (input.IsKeyPressed( 1 )) {
+            break;
+        }
         input.Reset();
         std::this_thread::sleep_for( std::chrono::milliseconds(100) );
     }
