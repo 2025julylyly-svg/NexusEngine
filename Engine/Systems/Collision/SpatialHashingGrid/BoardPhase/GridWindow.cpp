efea02f0b5d65b5ef6163317d90aefb56b98e6c1
Develepment <2025julylyly@gmail.com>
2026-08-20 23:10:35 +0330

    fixed bug in class CreateGridWindow for AddShape, CreateGrid Functions.

--- Engine/Systems/Collision/SpatialHashingGrid/BoardPhase/GridWindow.cpp
+++ Engine/Systems/Collision/SpatialHashingGrid/BoardPhase/GridWindow.cpp
@@ -23,7 +23,7 @@ Vector<Vector<CreateGridWindow::cell>> CreateGridWindow::CreateGrid(const Vector
         for (int point = 0; point < shape->getPointCount(); ++point) {
             const float X = shape->getPoint(static_cast<std::size_t>(point)).x;
             const float Y = shape->getPoint(static_cast<std::size_t>(point)).y;
-            GRID[static_cast<int>(Y) / CELL_SIZE][static_cast<int>(X) / CELL_SIZE] = cell(X, Y, shape);
+            GRID[static_cast<int>(Y) / CELL_SIZE][static_cast<int>(X) / CELL_SIZE].shapes.Append( { .x = X, .y = Y, .PTR_TO_SHAPE = shape } );
         }
     }
     return GRID;
@@ -32,7 +32,7 @@ void CreateGridWindow::AddShape(Shape* shape) {
     for (int point = 0; point < shape->getPointCount(); ++point) {
         const float X = (shape->getPoint(static_cast<std::size_t>(point)).x);
         const float Y = (shape->getPoint(static_cast<std::size_t>(point)).y);
-        cells[static_cast<int>(Y) / CELL_SIZE][static_cast<int>(X) / CELL_SIZE] = cell(X, Y, shape);
+        cells[static_cast<int>(Y) / CELL_SIZE][static_cast<int>(X) / CELL_SIZE].shapes.Append( { .x = X, .y = Y, .PTR_TO_SHAPE = shape } );
     }
 }
 
