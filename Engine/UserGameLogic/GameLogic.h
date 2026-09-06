05a267ac6ee552cc174ab655396a81a06e42e15a
Develepment <2025julylyly@gmail.com>
2026-09-06 21:33:29 +0330

    Add GameLogic class documentation and interface

--- Engine/UserGameLogic/GameLogic.h
+++ Engine/UserGameLogic/GameLogic.h
@@ -3,7 +3,15 @@
 #include "../Systems/Input/InputSystemFiles/ReadInput/ReadInputKey.h"
 
 class WS;
-
+/**
+ * @class GameLogic
+ * @brief Abstract base class for game logic implementation.
+ *
+ * This class defines the interface for game logic, including setting dependencies and
+ * the main game loop.
+ *
+ * @note Concrete implementations of this class should provide a definition for the `Main` method.
+ */
 class GameLogic
 {
 protected:
